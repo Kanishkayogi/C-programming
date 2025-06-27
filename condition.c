ad5edfa238dc89f7adbd6ed2ab9405671bdc25f2
kanishkayogi <kanishkayogi18@gmail.co>
2025-06-27 16:58:40 +0000

    condition.c

---
+++ condition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int num;
+   printf("Enter the number:");
+    scanf("%d", &num);
+    
+    if(num % 2 == 0){
+        printf("This is an even number\n");
+    } else {
+        printf("This is an odd number\n");
+}
+    return 0;
+}
